8a68ae134d06f63c5e71b66eb3c9fe55ded9fbf2
niley7464 <jyl7464@gmail.com>
2018-12-27 21:41:09 +0900

    tab

--- app/src/main/java/com/example/q/myapplication/MainActivity.java
+++ app/src/main/java/com/example/q/myapplication/MainActivity.java
@@ -90,41 +90,7 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.tab1, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-    }
-
+   //deleted PlaceholderFragment class from here
     /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
      * one of the sections/tabs/pages.
@@ -137,9 +103,20 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+           switch(position){
+               case 0 :
+                   Tab1 tab1 = new Tab1();
+                   return tab1;
+               case 1 :
+                   Tab2 tab2 = new Tab2();
+                   return tab2;
+               case 2 :
+                   Tab3 tab3 = new Tab3();
+                   return tab3;
+                   default:
+                       return null;
+
+           }
         }
 
         @Override
@@ -147,5 +124,46 @@ public class MainActivity extends AppCompatActivity {
             // Show 3 total pages.
             return 3;
         }
+
+        @Override
+        public CharSequence getPageTitle(int position)
+        {
+            switch(position){
+                case 0 :
+                    return "SECTION 1";
+                case 1 :
+                    return "SECTION 2";
+                case 2 :
+                    return "SECTRION 3";
+            }
+            return null;
+        }
     }
 }
+/*
+public static class PlaceholderFragment extends Fragment {
+
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    public PlaceholderFragment() {
+    }
+
+
+    public static PlaceholderFragment newInstance(int sectionNumber) {
+        PlaceholderFragment fragment = new PlaceholderFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab1, container, false);
+        TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+        textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+        return rootView;
+    }
+}
+*/
--- app/src/main/java/com/example/q/myapplication/Tab1.java
+++ app/src/main/java/com/example/q/myapplication/Tab1.java
@@ -1,4 +1,17 @@
 package com.example.q.myapplication;
 
-public class Tab1 {
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab1  extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab1, container, false);
+        return rootView;
+    }
 }
