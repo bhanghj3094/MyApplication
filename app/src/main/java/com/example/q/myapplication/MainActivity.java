3bffa234065b961ac24becdef5d90212143daa0c
bhanghj3094 <bhanghj3094@gmail.com>
2018-12-29 23:13:22 +0900

    Tag2: 갤러리에서 사진 불러오기, PhotoView로 확대

--- app/src/main/java/com/example/q/myapplication/MainActivity.java
+++ app/src/main/java/com/example/q/myapplication/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.q.myapplication;
 
 import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -11,13 +9,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private SectionsPagerAdapter mSectionsPagerAdapter;
@@ -48,6 +40,8 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
+
    //deleted PlaceholderFragment class from here
     /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
